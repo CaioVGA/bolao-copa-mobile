8aef55ae4daef3161efd0a2cc63ece6570815fa5
CaioVGA <caio.v.araujo1997@hotmail.com>
2022-11-10 18:33:52 -0300

    Features: Sign in + authentication with OAuth.

---
+++ src/components/button.tsx
@@ -0,0 +1,37 @@
+import { Button as ButtonNativeBase, Text, IButtonProps } from 'native-base';
+
+interface Props extends IButtonProps {
+    title: string;
+    type?: 'PRIMARY' | 'SECONDARY';
+}
+
+
+export function Button({ title, type='PRIMARY', ...rest }: Props) {
+    return (
+        <ButtonNativeBase 
+            w="full"
+            h={14}
+            rounded="sm"
+            fontSize="md"
+            textTransform="uppercase"
+            bg={type === 'SECONDARY' ? 'red.500' : 'red.300'}
+            _pressed={{
+                bg:type === 'SECONDARY' ? 'red.600' : 'red.700'
+            }}
+            _loading={{
+                _spinner: { color:'white' }
+            }}
+
+            {...rest}
+        >
+            <Text
+                fontSize="sm"
+                fontFamily="heading"
+                color={type === 'SECONDARY' ? 'white' : 'gray.100'}
+
+            >
+                {title}
+            </Text>
+        </ButtonNativeBase>
+    );
+}
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,31 @@
+// import { StatusBar } from "expo-status-bar";
+import { Center, Text, Icon } from "native-base";
+import { Fontisto } from '@expo/vector-icons';
+import { useAuth } from '../hooks/useAuth';
+
+import Logo from '../assets/logo-copa.svg';
+
+import { Button } from '../components/button';
+
+export function SignIn() {
+    
+  const { signIn, user } = useAuth();
+
+  return (
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={200} height={200} />
+
+      <Button 
+        title="ENTRAR COM GOOGLE" 
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        type="SECONDARY"
+        mt={6}
+        onPress={signIn}
+      />
+      <Text color="gray.200" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'}
+        do seu e-mail para criação de sua conta.
+      </Text>
+    </Center>
+  )
+}
