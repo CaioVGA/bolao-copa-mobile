e6b1fd9279e9a240f3fb97531f2cc2bfea7cd628
CaioVGA <caio.v.araujo1997@hotmail.com>
2022-11-11 01:34:32 -0300

    Datails and labels

--- src/screens/Pools.tsx
+++ src/screens/Pools.tsx
@@ -8,7 +8,7 @@ import { Header } from '../components/Header';
 export function Pools() {
     return (
         <VStack flex={1} bgColor="gray.900">
-            <Header title="Meus Bolões" />
+            <Header title="Meus bolões" />
 
             <VStack mt={6} mx={5} borderBottomWidth={1} borderBottomColor="gray.600" pb={4} mb={4}>
                 <Button 
